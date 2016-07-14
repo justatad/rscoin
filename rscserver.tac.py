c620b60edc30d2dc2b6d16501c72a44a42a05d49
Matthew D. Sheppard <me@matthewsheppard.co.uk>
2016-07-14 17:05:52 -0400

    Rewrite of rscserver.tac.py to use new factory for epoch end

--- rscserver.tac.py
+++ rscserver.tac.py
@@ -3,15 +3,22 @@ from twisted.application.internet import TimerService
 from rscoin.rscservice import RSCFactory, load_setup
 from twisted.internet.endpoints import TCP4ClientEndpoint
 from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
 
 import rscoin
 from base64 import b64encode, b64decode
 
-def CloseEpoch():
+class closeEpochMessage(Protocol):
+    def sendMessage(self, msg):
+        self.transport.write(msg)
 
+def gotProtocol(p):
+    p.sendMessage("xCloseEpoch")
+
+def CloseEpoch():
     point = TCP4ClientEndpoint(reactor, "127.0.0.1", 8080, timeout=10)
-    d = point.connect(f)
-    d.sendLine("xCloseEpoch")
+    d = connectProtocol(point, closeEpochMessage())
+    d.addCallback(gotProtocol)
 
 secret = file("secret.key").read()
 public = rscoin.Key(secret, public=False)
