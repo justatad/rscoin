ad74753d313448b99035e1a035f96ea9cd8bb4e7
Matthew Sheppard <mds@Matthews-iMac.local>
2016-07-22 15:25:57 +0100

    Corrected typo

--- centralbankserver.tac.py
+++ centralbankserver.tac.py
@@ -7,5 +7,5 @@ from base64 import b64encode, b64decode
 
 
 application = service.Application("rscoin")
-ts = TimerService(60, Central_Bank.print_message())
+ts = TimerService(60, Central_Bank.print_messages())
 ts.setServiceParent(application)
