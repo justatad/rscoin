3ff034039525e96e3d9150f4c2f8d720dd615ee8
Ubuntu <ubuntu@ip-172-30-0-117.eu-west-1.compute.internal>
2016-07-30 11:18:33 +0000

    Fixed to provide centralbank with secret key

--- centralbankserver.tac.py
+++ centralbankserver.tac.py
@@ -7,9 +7,9 @@ from base64 import b64encode, b64decode
 
 dir_data = file("directory.conf").read()
 directory = load_setup(dir_data)
-this_bank = Central_Bank(directory["directory"])
 secret = file("secret.key").read()
+this_bank = Central_Bank(directory["directory"], secret)
 
 application = service.Application("rscoin")
-ts = TimerService(1, this_bank.process_lower_blocks, secret)
+ts = TimerService(1, this_bank.process_lower_blocks)
 ts.setServiceParent(application)
